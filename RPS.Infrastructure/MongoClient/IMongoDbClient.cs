0385ce4a1880d94b705b78bed1da16cdd0010ebe
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-01-25 17:53:03 +0300

    mongodb client and features added

---
+++ RPS.Infrastructure/MongoClient/IMongoDbClient.cs
@@ -0,0 +1,16 @@
+﻿using RPS.Shared.Rating;
+
+namespace RPS.Infrastructure.MongoClient;
+
+public interface IMongoDbClient
+{
+    public Task<List<UserRatingMongoDto>> GetAsync();
+
+    public Task<UserRatingMongoDto> GetAsync(string key);
+
+    public Task CreateAsync(UserRatingMongoDto newMetadata);
+
+    public Task UpdateAsync(string key, UserRatingMongoDto updatedMetadata);
+
+    public Task RemoveAsync(string key);
+}
