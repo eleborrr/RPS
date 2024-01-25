a09313ccbc2c47c7f3821723a91ab12a6cebbe9c
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-01-25 22:11:41 +0300

    added entity for match, handlers for match, changed hub

--- RPS.Application/Features/Match/GetMatchResult/GetMatchResultQueryHandler.cs
+++ RPS.Application/Features/Match/GetMatchResult/GetMatchResultQueryHandler.cs
@@ -9,6 +9,10 @@ namespace RPS.Application.Features.Match.GetMatchResult;
 public class GetMatchResultQueryHandler: IQueryHandler<GetMatchResultQuery, MatchResultDto>
 {
     private readonly IRepositoryManager _repositoryManager;
+    private const string RockId = "1";
+    private const string PaperId = "2";
+    private const string ScissorsId = "3";
+
 
     public GetMatchResultQueryHandler(IRepositoryManager repositoryManager, IServiceManager serviceManager)
     {
@@ -19,15 +23,31 @@ public class GetMatchResultQueryHandler: IQueryHandler<GetMatchResultQuery, Matc
     {
         var match = await _repositoryManager.MatchRepository.GetByMatchIdAsync(request.MatchId);
         
-        //TODO обработка несущсетсвующего матча
         if (match is null)
             return new Result<MatchResultDto>(null, false, $"Match with id {request.MatchId} not exists");
 
         if (match.FirstUserMoveId == match.SecondUserMoveId)
             return new Result<MatchResultDto>(null, true);
         
-        var 
-        
-        if
+        //TODO захардкоженная проверка на id. Че с ними делать?
+        switch (match)
+        {
+            case {FirstUserMoveId: ScissorsId, SecondUserMoveId: PaperId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.FirstUserId), true);
+            case {FirstUserMoveId: PaperId, SecondUserMoveId: ScissorsId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.SecondUserId), true);
+            
+            case {FirstUserMoveId: RockId, SecondUserMoveId: ScissorsId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.FirstUserId), true);
+            case {FirstUserMoveId: ScissorsId, SecondUserMoveId: RockId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.SecondUserId), true);
+            
+            case {FirstUserMoveId: PaperId, SecondUserMoveId: RockId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.FirstUserId), true);
+            case {FirstUserMoveId: RockId, SecondUserMoveId: PaperId}: 
+                return new Result<MatchResultDto>(new MatchResultDto(match.SecondUserId), true);
+        }
+
+        return new Result<MatchResultDto>(null, false, "Something went wrong. Try again later.");
     }
 }
--- RPS.Domain/Repositories.Abstractions/IMatchRepository.cs
+++ RPS.Domain/Repositories.Abstractions/IMatchRepository.cs
@@ -7,4 +7,5 @@ public interface IMatchRepository
     public Task<IEnumerable<Match>> GetAllAsync(CancellationToken cancellationToken);
     public Task AddAsync(Match match);
     public Task<Match?> GetByMatchIdAsync(string matchId);
+    public Task UpdateAsync(Match match);
 }
