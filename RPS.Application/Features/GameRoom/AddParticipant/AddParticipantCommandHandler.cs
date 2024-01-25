d2a3f6aabcd081a99d890628d6cf461d4b0f31b8
Gleb Petukhov <gleb-petuhov@mail.ru>
2024-01-25 23:35:04 +0300

    [add] add participant for game room feature; [edit] group name value in GameRoomHub

---
+++ RPS.Application/Features/GameRoom/AddParticipant/AddParticipantCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using BeaverTinder.Application.Services.Abstractions.Cqrs.Commands;
+using RPS.Application.Dto.MediatR;
+using RPS.Domain.Repositories.Abstractions;
+
+namespace RPS.Application.Features.GameRoom.AddParticipant;
+
+public class AddParticipantCommandHandler: ICommandHandler<AddParticipantCommand>
+{
+    private readonly IRepositoryManager _repositoryManager;
+
+    public AddParticipantCommandHandler(IRepositoryManager repositoryManager)
+    {
+        _repositoryManager = repositoryManager;
+    }
+
+    public async Task<Result> Handle(AddParticipantCommand request, CancellationToken cancellationToken)
+    {
+        var gameRoom = await _repositoryManager.GameRoomRepository.GetByGameRoomIdAsync(request.GameRoomId);
+
+        if (gameRoom.Participant1 is null)
+            gameRoom.Participant1 = request.ParticipantId;
+        else if (gameRoom.Participant2 is null)
+            gameRoom.Participant2 = request.ParticipantId;
+        else
+            return new Result(false, "Cannot join this game. Lobby is full!");
+        await _repositoryManager.GameRoomRepository.UpdateAsync(gameRoom);
+        return new Result(true);
+    }
+}
